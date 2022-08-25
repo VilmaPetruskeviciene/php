b712b009bafa1f98422f6ee44527a700b6a02abb
VilmaPetruskeviciene <105633730+VilmaPetruskeviciene@users.noreply.github.com>
2022-08-25 13:27:24 +0300

    object

---
+++ 025/MarsoPalydovas.php
@@ -0,0 +1,21 @@
+<?php
+namespace Kosmosas;
+
+class MarsoPalydovas {
+    private static $palydovai = [null, null];
+    public $name;
+
+    static function create() {
+        if(null === self::$palydovai[0]) {
+           return self::$palydovai[0] = new self('Deimas');
+        }
+        if(null === self::$palydovai[1]) {
+            return self::$palydovai[1] = new self('Fobas');
+         }
+         return self::$palydovai[rand(0, 1)];
+    }
+
+    private function __construct($name) {
+        $this->name = $name;
+    }
+}
---
+++ 025/Tenisininkas.php
@@ -0,0 +1,65 @@
+<?php
+
+
+class Tenisininkas {
+
+    private $vardas, $kamuoliukas;
+
+    static private $zaidejas1, $zaidejas2;
+
+    static public function zaidimoPradzia()
+    {
+        if (self::$zaidejas1 === null || self::$zaidejas2 === null) {
+            echo '<h1>A kur Zaidėjai? A?</h1>';
+            return;
+        }
+        if (rand(0, 1)) {
+            self::$zaidejas1->kamuoliukas = false;
+            self::$zaidejas2->kamuoliukas = true;
+        } else {
+            self::$zaidejas2->kamuoliukas = false;
+            self::$zaidejas1->kamuoliukas = true;
+        }
+
+    }
+
+    public function __construct($vardas)
+    {
+        $this->vardas = $vardas;
+        if (self::$zaidejas1 === null) {
+            self::$zaidejas1 = $this;
+        }
+        else if (self::$zaidejas2 === null) {
+            self::$zaidejas2 = $this;
+        }
+    }
+
+    public function arTuriKamuoliuka()
+    {
+        return $this->kamuoliukas;
+    }
+
+    public function perduotiKamuoliuka()
+    {
+        if ($this->kamuoliukas) {
+            if (self::$zaidejas1->vardas == $this->vardas) {
+
+                self::$zaidejas1->kamuoliukas = false;
+                self::$zaidejas2->kamuoliukas = true;
+
+            } else if (self::$zaidejas2->vardas == $this->vardas) {
+
+                self::$zaidejas2->kamuoliukas = false;
+                self::$zaidejas1->kamuoliukas = true;
+                
+            } else {
+                echo '<h1>Ziūrovas</h1>';
+            }
+        } else {
+            echo '<h1>Neturi Kamuoliuko. Vo.</h1>';
+        }
+    }
+
+    
+
+}
